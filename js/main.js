c6fac5eb40d931691129d417cdc8cd248c301414
Simon Friis Vindum <simonfv@gmail.com>
2013-09-14 20:15:50 +0200

    Finish blog design

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ jQuery(function($) {
   var $sidebar = $('aside')
   $(window).scroll(function() {
     var offset = $(window).scrollTop()
-    console.log(offset)
-    if ($sidebar.css('border-left-width') == '1px') {
+    if ($(window).width() > 780) {
       $sidebar.css('marginTop', offset * 0.5 + 'px')
     } else {
       $sidebar.css('marginTop', '0px')
